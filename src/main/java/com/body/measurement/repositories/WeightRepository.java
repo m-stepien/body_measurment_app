c473c5166c33260b1db3b13d65c163be9742e50d
Mateusz Stępień <mstepien7@protonmail.com>
2024-10-04 12:12:35 +0200

    trying different solution for bug in chart lib

--- src/main/java/com/body/measurement/repositories/WeightRepository.java
+++ src/main/java/com/body/measurement/repositories/WeightRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 public interface WeightRepository extends JpaRepository<Weight, Long> {
     Optional<Weight> findTopByOrderByDateDesc();
-    @Query("SELECT w FROM Weight w WHERE date<=:startDate AND date>=:endDate")
+    @Query("SELECT w FROM Weight w WHERE date>=:startDate AND date<=:endDate")
     List<Weight> findByDateBetween(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate stopDate);
     Optional<Weight> findTopByOrderByDateAsc();
     Optional<Weight> findByDate(LocalDate date);
--- src/main/resources/static/js/chartscript.js
+++ src/main/resources/static/js/chartscript.js
@@ -1,207 +1,226 @@
 let currentChart;
+const ctx = document.getElementById('weightChart').getContext('2d');
+    async function getWeightBefore(start){
+        const url= 'http://localhost:8080/weight/get/before?date='+encodeURIComponent(start);
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+              throw new Error(`Response status: ${response.status}`);
+            }
+            const weight = await response.json();
+            console.log("Data fetched successfully:", weight);
+            if(weight===null || weight.date===null){
+            return null;
+            }
+            else{
+            return weight;
+            }
+          } catch (error) {
+                console.error(error.message);
+            return null;
+          }
+    }
 
-async function getWeightBefore(start){
-    const url= 'http://localhost:8080/weight/get/before?date='+encodeURIComponent(start);
-    try {
-        const response = await fetch(url);
+    async function getWeightAfter(end){
+        const url= 'http://localhost:8080/weight/get/after?date='+encodeURIComponent(end);
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+              throw new Error(`Response status: ${response.status}`);
+            }
+            const weight = await response.json();
+            console.log("Data fetched successfully:", weight);
+            if(weight===null || weight.date===null){
+                return null;
+            }
+            else{
+                return weight;
+            }
+          } catch (error) {
+                console.error(error.message);
+            return null;
+          }
+    }
+
+    async function getWeightBetweenDates(start, end){
+          const url = 'http://localhost:8080/weight/get/betweendates?start='+encodeURIComponent(start)+'&end='+encodeURIComponent(end);
+          console.log("url " + url);
+        try {
+            const response = await fetch(url);
         if (!response.ok) {
           throw new Error(`Response status: ${response.status}`);
         }
-        const weight = await response.json();
-        console.log("Data fetched successfully:", weight);
-        return weight;
+        const weightList = await response.json();
+            console.log("Data fetched successfully:", weightList);
+        return weightList;
       } catch (error) {
             console.error(error.message);
         return null;
       }
-}
+    }
 
-async function getWeightAfter(end){
-    const url= 'http://localhost:8080/weight/get/after?date='+encodeURIComponent(end);
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
+
+    async function getFirstWeightData(){
+          const url = "http://localhost:8080/weight/get/first";
+              try {
+                  const response = await fetch(url);
+              if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+              }
+              const weight = await response.json();
+               console.log("Data fetched successfully:", weight);
+              return weight;
+            } catch (error) {
+              console.error(error.message);
+              return null;
+            }
+    }
+
+    async function getLastWeightData(){
+          const url = "http://localhost:8080/weight/get/last";
+              try {
+                  const response = await fetch(url);
+              if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+              }
+              const weight = await response.json();
+                  console.log("Data fetched successfully:", weight);
+              return weight;
+            } catch (error) {
+              console.error(error.message);
+              return null;
+            }
+
+    }
+
+    function calculateMinValueInChart(arrayWeight){
+        var min = Math.min(...arrayWeight);
+        var mod = min % 10;
+        var chartMin;
+        if(mod>=5){
+            chartMin = Math.floor(min/10)*10-10;
         }
-        const weight = await response.json();
-        console.log("Data fetched successfully:", weight);
-        return weight;
-      } catch (error) {
-            console.error(error.message);
-        return null;
-      }
-}
-
-async function getWeightBetweenDates(start, end){
-      const url = 'http://localhost:8080/weight/get/betweendates?start='+encodeURIComponent(start)+'&end='+encodeURIComponent(end);
-      console.log("url " + url);
-    try {
-        const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    const weightList = await response.json();
-        console.log("Data fetched successfully:", weightList);
-    return weightList;
-  } catch (error) {
-        console.error(error.message);
-    return null;
-  }
-}
-
-
-async function getFirstWeightData(){
-      const url = "http://localhost:8080/weight/get/first";
-          try {
-              const response = await fetch(url);
-          if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-          }
-          const weight = await response.json();
-           console.log("Data fetched successfully:", weight);
-          return weight;
-        } catch (error) {
-          console.error(error.message);
-          return null;
+        else{
+            chartMin = Math.floor(min/10)*10-5;
         }
-}
+        return chartMin;
+    }
 
-async function getLastWeightData(){
-      const url = "http://localhost:8080/weight/get/last";
-          try {
-              const response = await fetch(url);
-          if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-          }
-          const weight = await response.json();
-              console.log("Data fetched successfully:", weight);
-          return weight;
-        } catch (error) {
-          console.error(error.message);
-          return null;
+    function calculateMaxValueInChart(arrayWeight){
+        var max = Math.max(...arrayWeight);
+        var mod = max % 10;
+        var chartMax;
+        if(mod>=5){
+            chartMax = Math.floor(max/10)*10+15;
         }
+        else{
+            chartMax = Math.floor(max/10)*10+10;
+        }
+        return chartMax;
+    }
+    function mapWeightOnDatesInChart(dates, weightList){
+        var weightForDates = dates.map(date =>{
+            const weightEntry = weightList.find(weight => date.getTime() === new Date(weight.date).getTime());
+            return {
+                date: date,
+                weight: weightEntry ? weightEntry.weightInKg : null
+            };
+            });
+        return weightForDates;
+    }
 
-}
+    function generateRecordForDates(startDateStr, endDateStr){
+        const dates = []
+        let currentDate = new Date(startDateStr);
+        let endDate = new Date(endDateStr);
+        console.log("I am in function");
+        console.log("current date" + currentDate);
+        console.log("end date" + endDate);
+        while(currentDate<=endDate){
+            console.log("Iam in loop");
+            dates.push(new Date(currentDate));
+            currentDate.setDate(currentDate.getDate() + 1);
+        }
+        return dates;
+    }
 
-function calculateMinValueInChart(arrayWeight){
-    var min = Math.min(...arrayWeight);
-    var mod = min % 10;
-    var chartMin;
-    if(mod>=5){
-        chartMin = Math.floor(min/10)*10-10;
-    }
-    else{
-        chartMin = Math.floor(min/10)*10-5;
-    }
-    return chartMin;
-}
-
-function calculateMaxValueInChart(arrayWeight){
-    var max = Math.max(...arrayWeight);
-    var mod = max % 10;
-    var chartMax;
-    if(mod>=5){
-        chartMax = Math.floor(max/10)*10+15;
-    }
-    else{
-        chartMax = Math.floor(max/10)*10+10;
-    }
-    return chartMax;
-}
-function mapWeightOnDatesInChart(dates, weightList){
-    var weightForDates = dates.map(date =>{
-        const weightEntry = weightList.find(weight => date.getTime() === new Date(weight.date).getTime());
+    async function generateChart(data, minV, maxV, minDate, maxDate) {
+        if (currentChart) {
+            currentChart.destroy();
+        }
+        let chartData = data.map(row => {
         return {
-            date: date,
-            weight: weightEntry ? weightEntry.weightInKg : null
-        };
-        });
-    return weightForDates;
-}
-
-function generateRecordForDates(){
-    const dates = []
-    var startDate = document.getElementById('startDate').value;
-    var endDate = new Date(document.getElementById("endDate").value);
-    let currentDate = new Date(startDate);
-    while(currentDate<=endDate){
-        dates.push(new Date(currentDate));
-        currentDate.setDate(currentDate.getDate() + 1);
-    }
-    return dates;
-}
-
-async function generateChart(data, minV, maxV, minDate, maxDate) {
-    if (currentChart) {
-        currentChart.destroy();
-    }
-  currentChart = new Chart(
-    document.getElementById('weightChart'),
-    {
-      type: 'line',
-      data: {
-        labels: data.map(row => row.date.toISOString().split('T')[0]),
-        datasets: [
-          {
-            label: 'Weight in kg',
-            data: data.map(row => row.weight),
-            spanGaps: true
-          }
-        ]
-      },
-      options: {
-                  scales: {
-                          x: {
-                              type: 'time',
-                              time: {
-                                  unit: 'day'
+            x: row.date.toISOString().split('T')[0],
+            y: row.weight
+        };});
+      currentChart = new Chart(
+        ctx,
+        {
+          type: 'line',
+          data: {
+            datasets: [
+              {
+                label: 'Weight in kg',
+                data: chartData,
+                spanGaps: true,
+                fill: false,
+                stepped: false,
+              }
+            ]
+          },
+          options: {
+                      scales: {
+                              x: {
+                                  type: 'time',
+                                  time: {
+                                      unit: 'day'
+                                  },
+                                  min: new Date(minDate).getTime(),
+                                  max: new Date(maxDate).getTime()
                               },
-                              min: new Date(minDate).getTime(),
-                              max: new Date(maxDate).getTime()
-                          },
-                          y: {
-                            min: minV,
-                            max: maxV
+                              y: {
+                                min: minV,
+                                max: maxV
+                              }
                           }
-                      }
-      }
+          }
+        }
+      );
     }
-  );
-}
 
 
-async function createChart(){
-    var startDateFromInput = document.getElementById('startDate').value;
-    var endDateFromInput = document.getElementById("endDate").value;
-    var oneBefore = await getWeightBefore(startDateFromInput);
-    var oneAfter = await getWeightAfter(endDateFromInput);
-    var dateStart;
-    var dateEnd;
-    if(oneBefore.date!==null){
-        console.log("+")
-        console.log(oneBefore);
-        dateStart = oneBefore.date;
+    async function createChart(){
+        var startDateFromInput = document.getElementById('startDate').value;
+        var endDateFromInput = document.getElementById("endDate").value;
+        var oneBefore = await getWeightBefore(startDateFromInput);
+        var oneAfter = await getWeightAfter(endDateFromInput);
+        var dateStart;
+        var dateEnd;
+        if(oneBefore!==null){
+            console.log("+")
+            console.log(oneBefore);
+            dateStart = oneBefore.date;
+        }
+        else{
+            dateStart = startDateFromInput;
+        }
+        if(oneAfter!==null){
+            console.log("+")
+            console.log(oneAfter);
+            dateEnd = oneAfter.date;
+        }
+        else{
+            dateEnd = endDateFromInput;
+        }
+        var weightList = await getWeightBetweenDates(dateStart, dateEnd);
+        var weights = weightList.map(weight => weight.weightInKg);
+        var datesForChart = generateRecordForDates(dateStart, dateEnd);
+        console.log("datesFordChart" + datesForChart);
+        var dataForChart = mapWeightOnDatesInChart(datesForChart, weightList);
+        generateChart(dataForChart, calculateMinValueInChart(weights), calculateMaxValueInChart(weights), startDateFromInput, endDateFromInput);
     }
-    else{
-        dateStart = startDateFromInput;
-    }
-    if(oneAfter.date!==null){
-        console.log("+")
-        console.log(oneAfter);
-        dateEnd = oneAfter.date;
-    }
-    else{
-        dateEnd = endDateFromInput;
-    }
-    console.log("start" + dateStart);
-    console.log("end" + dateEnd);
-    var weightList = await getWeightBetweenDates(dateStart, dateEnd);
-    var weights = weightList.map(weight => weight.weightInKg);
-    var datesForChart = generateRecordForDates();
-    var dataForChart = mapWeightOnDatesInChart(datesForChart, weightList);
-    generateChart(dataForChart, calculateMinValueInChart(weights), calculateMaxValueInChart(weights));
-}
 
-(async function(){
-    createChart();
-})();
+    (async function(){
+        createChart();
+    })();
+
