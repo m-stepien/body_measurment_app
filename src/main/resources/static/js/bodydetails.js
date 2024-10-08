960acb5200ed6646d8c1fe6cec5ea0a0302fe905
Mateusz Stępień <mstepien7@protonmail.com>
2024-10-08 16:15:55 +0200

    fetching body datails in side for updating body details

---
+++ src/main/resources/static/js/bodydetails.js
@@ -0,0 +1,38 @@
+async function getBasicBodyData() {
+  const url = "http://localhost:8080/body/basic/get/1";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const bodyData = await response.json();
+    console.log("Data fetched successfully:", bodyData);
+    return bodyData;
+  } catch (error) {
+    return null;
+  }
+}
+
+function putInsideDom(bodyDetails){
+    console.log(bodyDetails);
+    let heightInput = document.getElementById("heightInCm")
+    heightInCm.value = bodyDetails.heightInCm;
+    let ageInput = document.getElementById("age");
+    ageInput.value = bodyDetails.age;
+    let genderInputId;
+    if(bodyDetails.gender ==="m"){
+        genderInputId = "male"
+    }
+    else if(bodyDetails.gender === "f"){
+        genderInputId = "female"
+    }
+    else if(bodyDetails.gender === "o"){
+        genderInputId = "other"
+    }
+    document.getElementById(genderInputId).checked = true;
+}
+
+(async ()=>{
+    let bodyDetails = await getBasicBodyData();
+    putInsideDom(bodyDetails);
+})();
